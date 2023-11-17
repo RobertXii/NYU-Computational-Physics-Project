b5f887345e13b846f6c58b788a54d1385b8813d8
robertXi6 <144391056+robertXi6@users.noreply.github.com>
2023-11-16 22:57:07 -0500

    update

--- Group Project/Higher Order 1D Method.py
+++ Group Project/Higher Order 1D Method.py
@@ -9,7 +9,10 @@ v0 = 0.01 # initial velocity
 delta_x = 1
 delta_t = 0.3
 gamma = 1.4
+theta = 1
 U = np.zeros((nx, 3))
+c_int = np.zeros((nx, 3))
+c_half = np.zeros((nx-2, 3))
 F = np.zeros((nx, 3))
 F_half = np.zeros((nx, 3))
 U_der = np.zeros((nx, 3))
@@ -24,6 +27,26 @@ for i in range(nx):
         rho_R = 0.1
         P_R = 0.1
         U[i, 0:3] = np.array([rho_R, rho_R*v0, P_R])
+
+def sign(n):
+    return n/abs(n)
+
+def minmod(x,y,z):
+    return 0.25* abs(sign(x) + sign(y))(sign(x) + sign(z))* min(abs(x), abs(y), abs(z))
+
+# def differences(list):
+#     return np.subtract(list[:][1:], list[:][:-1])
+#
+# def differences2(list):
+#     return np.subtract(list[:][2:], list[:][:-2])
+#
+# def find_c_half(U):
+#     c_int[:, 0] = U[:, 0]
+#     c_int[:, 1] = (gamma-1)*U[:, 0]*(U[:, 2]/U[:, 0]-0.5*(U[:, 1]/U[:, 0])**2)
+#     c_int[:, 2] = U[:, 1]/U[:, 0]
+#     diff = differences(c_int)
+#     diff2 = differences(c_int)
+#     c_half[:, :] = c_int[1:-1, :] + 0.5 * minmod(theta*diff[:-1,:], 0.5*diff2, theta*diff[:-1,:])
 
 def find_f(U):
     v = U[:, 1] / U[:,  0]
@@ -58,11 +81,9 @@ def find_u_der(F_half, delta_x):
     # print(U_der)
     return U_der
 
-def find_u(U, U_der,delta_t,ii):
+def find_u(U, U_der,delta_t):
     # print(U)
     # U_histor[i] = U[:nx-2,:,:]
-    if ii % 10 == 0:
-        plt.plot(range(nx), U[:,1]/U[:,0])
     U[:,:]= U[:,:] + delta_t*U_der
     # print(U)
     return U
@@ -71,6 +92,15 @@ for i in range(nt):
     F = find_f(U)
     F_half = find_f_half(U,F)
     U_der = find_u_der(F_half,delta_x)
-    U = find_u(U,U_der,delta_t,i)
+    U1 = find_u(U,U_der,delta_t)
+    F_half2 = find_f_half(U1,F)
+    U_der2 = find_u_der(F_half2,delta_x)
+    U2 = 0.75*U+0.25*U1+0.25*delta_t*U_der2
+    F_half3 = find_f_half(U2,F)
+    U_der3 = find_u_der(F_half3,delta_x)
+    U = 1/3*U+2/3*U2+2/3*delta_t*U_der3
+    if i % 10 == 0:
+        plt.plot(range(nx), U[:,0])
 
+plt.title('position vs density')
 plt.show()
